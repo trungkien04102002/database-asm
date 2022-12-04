a43a69086fafa105c0e0e14828104f74cd107b51
Hoang39 <87194190+Hoang39@users.noreply.github.com>
2022-12-04 21:54:22 +0700

    Update Section 1.2.3.sql

--- SQL Script/Section 1.2.3.sql
+++ SQL Script/Section 1.2.3.sql
@@ -14,11 +14,33 @@ BEGIN
     END IF;
     
     IF message_error = '' THEN
-        SELECT dishName FROM Dish
+        SELECT dishName AS Name FROM Dish
 	WHERE resID = $ResID
 	ORDER BY dishName ASC;
     ELSE
 	SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = message_error;
     END IF;
 END;|
+
+-- Thủ tục lấy số khách hàng đặt hàng thành công ở các nhà hàng
+
+DROP PROCEDURE IF EXISTS Count_Customer_Order_Success|
+CREATE PROCEDURE Count_Customer_Order_Success(
+    IN ResID INT
+)
+BEGIN
+    DECLARE message_error VARCHAR(1000) DEFAULT '';
+    IF (NOT EXISTS (SELECT * FROM Restaurant WHERE resID = $ResID)) THEN
+		SET message_error = 'THIS RESID DOES NOT EXIST!';
+    END IF;
+    
+    IF message_error = '' THEN
+        SELECT COUNT(c.userID) 
+	FROM Customer c, Orders o, Branch b
+	WHERE o.state = 'ready' AND o.branchID = b.branchID AND o.cusID = c.userID AND b.resID = $ResID;
+    ELSE
+	SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = message_error;
+    END IF;
+END;|
+
 delimiter ;
