c04c6721936abf6b70455454573c93fc8567ba7a
Hoang39 <87194190+Hoang39@users.noreply.github.com>
2022-12-04 22:12:59 +0700

    Update Section 1.2.3.sql

--- SQL Script/Section 1.2.3.sql
+++ SQL Script/Section 1.2.3.sql
@@ -25,22 +25,15 @@ END;|
 -- Thủ tục lấy số khách hàng đặt hàng thành công ở các nhà hàng
 
 DROP PROCEDURE IF EXISTS Count_Customer_Order_Success|
-CREATE PROCEDURE Count_Customer_Order_Success(
-    IN ResID INT
-)
+CREATE PROCEDURE Count_Customer_Order_Success()
 BEGIN
-    DECLARE message_error VARCHAR(1000) DEFAULT '';
-    IF (NOT EXISTS (SELECT * FROM Restaurant WHERE resID = $ResID)) THEN
-		SET message_error = 'THIS RESID DOES NOT EXIST!';
-    END IF;
-    
-    IF message_error = '' THEN
-        SELECT COUNT(DISTINCT c.userID) 
-	FROM Customer c, Orders o, Branch b
-	WHERE o.state = 'ready' AND o.branchID = b.branchID AND o.cusID = c.userID AND b.resID = $ResID;
-    ELSE
-	SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = message_error;
-    END IF;
+
+    SELECT r.resName AS Name, COUNT(DISTINCT c.userID) AS CountCustomer
+    FROM Customer c, Orders o, Branch b, Restaurant r
+    WHERE o.state = 'ready' AND o.branchID = b.branchID AND o.cusID = c.userID AND b.resID = $ResID
+    GROUP BY r.resName
+    HAVING r.resID = $ResID;
+   
 END;|
 
 delimiter ;
@@ -52,8 +45,4 @@ CALL List_Dish_Of_Restaurant(3);
 CALL List_Dish_Of_Restaurant(4);
 CALL List_Dish_Of_Restaurant(5);
 
-CALL Count_Customer_Order_Success(1);
-CALL Count_Customer_Order_Success(2);
-CALL Count_Customer_Order_Success(3);
-CALL Count_Customer_Order_Success(4);
-CALL Count_Customer_Order_Success(5);
+CALL Count_Customer_Order_Success();
