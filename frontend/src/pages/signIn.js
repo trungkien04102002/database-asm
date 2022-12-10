968dce0c436bd711aac4bc23afb9b70f6a776eff
Hoang39 <zmeb030902@gmail.com>
2022-12-10 15:51:28 +0700

    frontend

---
+++ frontend/src/pages/signIn.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SignInForm from '../components/Login/signInForm';
+
+const SignIn = () => {
+    return (
+        <>
+            <div className="bg-gradient-to-r from-orange-100 to-orange-200 z-[-100]">
+                <SignInForm />
+            </div>
+        </>
+    );
+}
+
+export default SignIn;
